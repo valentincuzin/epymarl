12c98514c9362634de39de217d1df0abd03f5da7
valentincuzin <valentincuzin.pro@ikmail.com>
2026-03-26 15:41:48 +0100

    going to pretrain tag

--- src/envs/pretrained/ddpg.py
+++ src/envs/pretrained/ddpg.py
@@ -2,39 +2,60 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MLP(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden, norm_in=True):
-        super(MLP, self).__init__()
-        self.input_dim = input_dim
-        self.output_dim = output_dim
+# class MLP(nn.Module):
+#     def __init__(self, input_dim, output_dim, hidden, norm_in=True):
+#         super(MLP, self).__init__()
+#         self.input_dim = input_dim
+#         self.output_dim = output_dim
 
-        # create network layers
-        if norm_in:  # normalize inputs
-            self.in_fn = nn.BatchNorm1d(input_dim)
-            self.in_fn.weight.data.fill_(1)
-            self.in_fn.bias.data.fill_(0)
-        else:
-            self.in_fn = lambda x: x
-        self.fc1 = nn.Linear(input_dim, hidden)
-        self.fc2 = nn.Linear(hidden, hidden)
-        self.fc3 = nn.Linear(hidden, output_dim)
+#         # create network layers
+#         if norm_in:  # normalize inputs
+#             self.in_fn = nn.BatchNorm1d(input_dim)
+#             self.in_fn.weight.data.fill_(1)
+#             self.in_fn.bias.data.fill_(0)
+#         else:
+#             self.in_fn = lambda x: x
+#         self.fc1 = nn.Linear(input_dim, hidden)
+#         self.fc2 = nn.Linear(hidden, hidden)
+#         self.fc3 = nn.Linear(hidden, output_dim)
 
-    def forward(self, x):
-        h = F.relu(self.fc1(self.in_fn(x)))
-        h = F.relu(self.fc2(h))
-        out = self.fc3(h)
-        return out
+#     def forward(self, x):
+#         print("x: ", x)
+#         h = F.relu(self.fc1(self.in_fn(x)))
+#         h = F.relu(self.fc2(h))
+#         out = self.fc3(h)
+#         return out
+
+class MLPAgent(nn.Module):
+    def __init__(self, input_shape, hidden_dim, n_actions):
+        super(MLPAgent, self).__init__()
+        self.hidden_dim = hidden_dim
+
+        self.fc1 = nn.Linear(input_shape, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, n_actions)
+        print(f"\n\nDEBUG: total number of PARAMETERS for MLPAgent: {sum(p.numel() for p in self.parameters())} #####\n\n")
+
+    def init_hidden(self):
+        # make hidden states on same device as model
+        return self.fc1.weight.new(1, self.hidden_dim).zero_()
+
+    def forward(self, inputs, hidden_state=None):
+        x = F.relu(self.fc1(inputs))
+        h = F.relu(self.fc2(x))
+        q = self.fc3(h)
+        return q, None
 
 class DDPG():
 
-    def __init__(self, actor_input_dim, actor_output_dim, critic_input_dim, hidden, lr):
-        self.policy = MLP(actor_input_dim, actor_output_dim, hidden)
+    def __init__(self, actor_input_dim, actor_output_dim, hidden):
+        self.policy = MLPAgent(actor_input_dim, actor_output_dim, hidden)
 
     def step(self, obs, explore=False):
-        obs = torch.Tensor(obs).unsqueeze(0)
+        obs = torch.Tensor(obs)
         action = self.policy(obs)
-        action = action.argmax().cpu().item()
-        return action
+        action = action.argmax(dim=1)
+        return action.cpu().numpy()
 
     def load_params(self, params):
         self.policy.load_state_dict(params['policy'])
--- src/envs/pretrained/tag.py
+++ src/envs/pretrained/tag.py
@@ -36,7 +36,6 @@ class RandomTag(gym.Wrapper):
     """Tag with pretrained prey agent"""
 
     def __init__(self, *args, **kwargs):
-        print(kwargs)
         super().__init__(*args, **{})
         self.num_good = kwargs["num_good"]
         self.num_adversaries = kwargs["num_adversaries"]
@@ -67,32 +66,34 @@ class PretrainedTag(gym.Wrapper):
     """Tag with pretrained prey agent"""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.pt_action_space = self.action_space[-1]
-        self.pt_observation_space = self.observation_space[-1]
-        self.action_space = Tuple(self.action_space[:-1])
-        self.observation_space = Tuple(self.observation_space[:-1])
-        self.n_agents = 3
-        self.unwrapped.n_agents = 3
+        super().__init__(*args, **{})
+        self.num_good = kwargs["num_good"]
+        self.num_adversaries = kwargs["num_adversaries"]
+        self.pt_action_space = self.action_space[-self.num_good:]
+        self.pt_observation_space = self.observation_space[-self.num_good:]
+        self.action_space = Tuple(self.action_space[:-self.num_good])
+        self.observation_space = Tuple(self.observation_space[:-self.num_good])
+        self.n_agents = self.num_adversaries
+        self.unwrapped.n_agents = self.num_adversaries
 
-        self.prey = DDPG(14, 5, 50, 128, 0.01)
+        self.preys = DDPG(16, 5, 128)
         # current file dir
         param_path = Path(__file__).parent / "prey_params.pt"
         save_dict = torch.load(param_path)
-        self.prey.load_params(save_dict["agent_params"][-1])
-        self.prey.policy.eval()
+        self.preys.load_params(save_dict["agent_params"][-1])
+        self.preys.policy.eval()
         self.last_prey_obs = None
 
     def reset(self, seed=None, options=None):
         obs, info = super().reset(seed=seed, options=options)
-        self.last_prey_obs = obs[-1]
-        return obs[:-1], info
+        self.last_prey_obs = obs[-self.num_good:]
+        return obs[:-self.num_good], info
 
     def step(self, action):
-        prey_action = self.prey.step(self.last_prey_obs)
+        prey_action = self.preys.step(self.last_prey_obs)
         action = tuple(action) + (prey_action,)
         obs, rew, done, truncated, info = super().step(action)
-        self.last_prey_obs = obs[-1]
-        obs = obs[:-1]
-        rew = rew[:-1]
+        self.last_prey_obs = obs[-self.num_good:]
+        obs = obs[:-self.num_good]
+        rew = rew[:-self.num_good]
         return obs, rew, done, truncated, info
